0fbfd80288ffd99486c03517264aa37455c0faaa
probuse <nprobuse@gmail.com>
2017-04-26 17:43:23 +0300

    added test for creating multiple living rooms

--- test_drabot.py
+++ test_drabot.py
@@ -10,12 +10,18 @@ class TestCreateRoom(unittest.TestCase):
         self.assertEqual(new_room_count - initial_room_count, 1)
         
     def test_multiple_rooms_created_successfully(self):
-        room = Room()
-        initial_room_count = len(room.room_names)
+        room2 = Room()
+        initial_room_count = len(room2.room_names)
         many_rooms = room.create_room('office', 'blue', 'black', 'red')
-        new_room_count = len(room.room_names)
+        new_room_count = len(room2.room_names)
         self.assertEqual(new_room_count - initial_room_count, 3)
-        
+    
+    def test_living_room_created_successfully(self):
+        room3 = Room()
+        initial_room_count = len(room3.room_names)
+        many_rooms = room.create_room('living space', 'blue', 'black', 'red')
+        new_room_count = len(room3.room_names)
+        self.assertEqual(new_room_count - initial_room_count, 3)
         
     
         
