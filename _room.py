8c10023dac09338338476140eca1391906fa0d9a
probuse <nprobuse@gmail.com>
2017-04-26 20:05:28 +0300

    Implemented logic of create_room

--- _room.py
+++ _room.py
@@ -8,9 +8,15 @@ class Room(object):
         Can create as many rooms as specified by room_name"""
         if room_type.lower() == 'office':
             for i in room_name:
+                if i in self.office_room_names:
+                    return 'Room {} already created'.format(i)
                 self.office_room_names.append(i)
+                print("An office called {} has been successfully created".format(i))
         elif room_type.lower() == 'living space':
             for i in room_name:
+                if i in self.office_room_names:
+                    return 'Room {} already created'.format(i)
                 self.living_room_names.append(i)
+                print("A Living space called {} has been successfully created".format(i))
         else:
             return "Room Type not in the Dojo"
