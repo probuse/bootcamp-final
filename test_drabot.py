c9723f68a7ac1436103e6f7b6aa89a2b497936c1
probuse <nprobuse@gmail.com>
2017-04-26 17:40:25 +0300

    New test case file for drabot

--- test_drabot.py
+++ test_drabot.py
@@ -4,23 +4,20 @@ from drabot import Room, Office, LivingSpace, Person, Fellow, Staff
 class TestCreateRoom(unittest.TestCase):
     def test_create_room_successfully(self):
         room1 = Room()
-        initial_room_count = len(room1.office_rooms) + len(room1.living_space_rooms)
-        orange = room1.create_room('office', 'Orange')
-        blue = room1.create_room('office', 'Blue')
-        self.assertTrue(orange)
-        new_room_count = len(room1.office_rooms) + len(room1.living_space_rooms)
-        self.assertEqual((new_room_count - initial_room_count), 2)
+        initial_room_count = len(room1.room_names)
+        python_room = room1.create_room('office', 'orange')
+        new_room_count = len(room1.room_names)
+        self.assertEqual(new_room_count - initial_room_count, 1)
+        
+    def test_multiple_rooms_created_successfully(self):
+        room = Room()
+        initial_room_count = len(room.room_names)
+        many_rooms = room.create_room('office', 'blue', 'black', 'red')
+        new_room_count = len(room.room_names)
+        self.assertEqual(new_room_count - initial_room_count, 3)
         
         
-    def test_add_staff_successfully(self):
-        room1 = Room()
-        initial_count = room1.total_staff()
-        staff1 = room1.add_person('Polly', 'staff')
-        staff2 = room1.add_person('Penny', 'staff')
-        new_count = room1.total_staff()
-        self.assertTrue(staff1)
-        self.assertTrue(staff2)
-        self.assertTrue(new_count - initial_count, 2)
+    
         
         
 
