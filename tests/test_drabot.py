b96b6151a14da1b4997f377670d726ca33bf79ae
probuse <nprobuse@gmail.com>
2017-04-28 07:39:51 +0300

    added test for persons age to be above 18 and name to be an aphabetical letter

--- tests/test_drabot.py
+++ tests/test_drabot.py
@@ -7,43 +7,41 @@ from person import Person
 
 
 class TestCreateRoom(unittest.TestCase):
+    def setup(self):
+        self.room = Room()
+    
     def test_create_room_successfully(self):
-        room1 = Room()
-        initial_room_count = len(room1.office_room_names)
-        python_room = room1.create_room('office', 'python')
-        new_room_count = len(room1.office_room_names)
+        office_name = self.room.create_room('office', 'havana')
+        initial_room_count = len(self.room.office_room_names)
+        python_room = self.room.create_room('office', 'python')
+        new_room_count = len(self.room.office_room_names)
         self.assertEqual(new_room_count - initial_room_count, 1)
         
     def test_multiple_rooms_created_successfully(self):
-        room2 = Room()
-        initial_room_count = len(room2.office_room_names)
-        many_rooms = room2.create_room('office', 'blue', 'black', 'red')
-        new_room_count = len(room2.office_room_names)
+        initial_room_count = len(self.room.office_room_names)
+        many_rooms = self.room.create_room('office', 'blue', 'black', 'red')
+        new_room_count = len(self.room.office_room_names)
         self.assertEqual(new_room_count - initial_room_count, 3)
     
     def test_living_room_created_successfully(self):
-        room3 = Room()
-        initial_room_count = len(room3.living_room_names)
-        many_rooms = room3.create_room('living space', 'blue', 'black', 'red')
-        new_room_count = len(room3.living_room_names)
+        initial_room_count = len(self.room.living_room_names)
+        many_rooms = self.room.create_room('living space', 'blue', 'black', 'red')
+        new_room_count = len(self.room.living_room_names)
         self.assertEqual(new_room_count - initial_room_count, 3)
     
     def test_add_person_successfully(self):
-        room4 = Room()
-        initial_andelan_count = room4.andelans
-        phillip = room4.add_person('Phillip', 'staff')
-        new_andelan_count = room4.andelans
+        initial_andelan_count = self.room.andelans
+        phillip = self.room.add_person('Phillip', 'staff')
+        new_andelan_count = self.room.andelans
         self.assertEqual(new_room_count - initial_room_count, 1)
-        
-    def test_person_exists_in_system(self):
-        room5 = Room()
-        pro = Fellow()
-        pro = room5.add_person('Pro', 'fellow')
-        self.assertIn('Pro', pro.fellow)
         
     def test_person_is_above_18(self):
         person = Person('probuse', 40)
         self.assertTrue(person.check_age())
+        
+    def test_false_if_person_is_below_18(self):
+        person = Person('Nuwa', 12)
+        self.assertFalse(person.check_age())
         
     def test_name_is_alpha(self):
         person = Person('Ray', 23)
